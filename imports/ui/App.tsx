f642a258747627c591ff6437a19e01ba01ffdbf4
Daniel Lamando <dan@danopia.net>
2024-03-22 23:40:21 +0100

    Remove error boundary

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,24 +1,13 @@
 import React, { StrictMode } from 'react';
-import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './router';
 
-const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => (
-  <div role="alert">
-    <p>Something went wrong:</p>
-    <pre>{error.message}</pre>
-    <button onClick={resetErrorBoundary}>Reset page</button>
-  </div>
-);
-
 export function App() {
   // const hasUser = useTracker(() => !!Meteor.userId());
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <StrictMode>
-        <RouterProvider router={router} fallbackElement={<progress />} />
-      </StrictMode>
-    </ErrorBoundary>
+    <StrictMode>
+      <RouterProvider router={router} fallbackElement={<progress />} />
+    </StrictMode>
   );
 }
