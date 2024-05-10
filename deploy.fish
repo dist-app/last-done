cfeda4695583ea1f7da9ce22a2c144eef58fe559
Daniel Lamando <dan@danopia.net>
2024-05-10 20:17:55 +0200

    Add a manifest artifact to the client bundle deploy scripts (#2)
    * Add manifest to uploaded artifact
    
    * we don't use web.browser.legacy

--- deploy.fish
+++ deploy.fish
@@ -7,6 +7,10 @@ meteor build --directory $tempdir
 cd $tempdir/bundle/programs
 ls -lsh web.browser
 set buildPrefix uber.danopia.net/meteor-bundles/last-done/$commit
+# inspired by https://stackoverflow.com/questions/73885999/how-to-create-a-json-file-with-jq
+# can probably break down further
+echo '{"web.browser":{"hashes":{"js":"'(basename web.browser/*.js .js)'","css":"'(basename web.browser/*.css .css)'"},"html":'(jq -cn --rawfile body web.browser/body.html --rawfile head web.browser/head.html '$ARGS.named')'},"server":'(jq -c . < server/config.json)'}' > manifest.json
+aws s3 cp --content-type "application/json; charset=utf-8" manifest.json s3://$buildPrefix/manifest.json
 aws s3 cp --content-type "application/javascript; charset=utf-8" web.browser/*.js s3://$buildPrefix/web.browser/app.js
 aws s3 cp --content-type "text/css; charset=utf-8" web.browser/*.css s3://$buildPrefix/web.browser/app.css
 echo https://$buildPrefix/web.browser/app.js
