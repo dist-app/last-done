a8dbf85105e799b9fa7e21c533231b7d82d63126
Daniel Lamando <dan@danopia.net>
2025-01-27 00:16:55 +0100

    React 19

--- client/main.tsx
+++ client/main.tsx
@@ -7,6 +7,13 @@ import { App } from '/imports/ui/App';
 
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
-  const root = createRoot(container!);
+  const root = createRoot(container!, {
+    // onUncaughtError: (error, errorInfo) => {
+    //   // ... log error report
+    // },
+    // onCaughtError: (error, errorInfo) => {
+    //   // ... log error report
+    // },
+  });
   root.render(<App />);
 });
