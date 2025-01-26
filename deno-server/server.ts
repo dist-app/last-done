da757ebb698889763cca8efbbae5c206f9182d1c
Daniel Lamando <dan@danopia.net>
2025-01-26 13:03:36 +0100

    See about using .ts for server imports

--- deno-server/server.ts
+++ deno-server/server.ts
@@ -1,8 +1,8 @@
 // Set up telemetry/tracer
-import "https://deno.land/x/observability@v0.6.1/preconfigured/from-environment.ts";
+import "https://deno.land/x/observability@v0.7.0/preconfigured/from-environment.ts";
 
 // Bring in the user-defined server logic
-import "../server/main";
+import "../server/main.ts";
 
 // Start up the app server
 import "dist-app-deno/hack/meteor-server/run.ts";
