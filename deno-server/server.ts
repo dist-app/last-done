d5ed25afcb2be2877ff16869400add9f7f6cc6c6
Daniel Lamando <dan@danopia.net>
2025-02-05 01:18:29 +0100

    New ci (#80)
    * New CI flow
    
    * Remove deno-server/server.ts

--- deno-server/server.ts
+++
@@ -1,8 +0,0 @@
-// Set up telemetry/tracer
-import "https://deno.land/x/observability@v0.7.0/preconfigured/from-environment.ts";
-
-// Bring in the user-defined server logic
-import "../server/main.ts";
-
-// Start up the app server
-import "dist-app-deno/hack/meteor-server/run.ts";
